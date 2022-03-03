4b730357b00b9e3b7dec02f6b8d3ba8a243c85cf
Vishal S Jadhav <100112614+VishalSJ97@users.noreply.github.com>
2022-03-03 21:10:49 +0530

    Update Digit Frequency.c

--- C/C (Basic)/Medium/Arrays and String/Digit Frequency.c
+++ C/C (Basic)/Medium/Arrays and String/Digit Frequency.c
@@ -6,49 +6,20 @@
 int main() {
 
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */
-    char c[100];
-    scanf("%s", c);
-    int f[10] = {0};
-    for(int i=0; i<strlen(c); i++){
-        switch(c[i]){
-            case '0':
-                f[0] = f[0] + 1;
-                break;
-            case '1':
-                f[1] = f[1] + 1;
-                break;
-            case '2':
-                f[2] = f[2] + 1;
-                break;
-            case '3':
-                f[3] = f[3] + 1;
-                break;
-            case '4':
-                f[4] = f[4] + 1;
-                break;
-            case '5':
-                f[5] = f[5] + 1;
-                break;
-            case '6':
-                f[6] = f[6] + 1;
-                break;
-            case '7':
-                f[7] = f[7] + 1;
-                break;
-            case '8':
-                f[8] = f[8] + 1;
-                break;
-            case '9':
-                f[9] = f[9] + 1;
-                break;
-            default:
-                break;
+    char a[1000];
+    int c,num[10] = {0};
+    scanf("%s",a);
+    for(int i = 0 ;i <= strlen(a);i++)
+    {
+        if(isdigit(a[i]) > 0)
+        {
+            c = (a[i]) - '0';
+            num[c] += 1;
         }
     }
-    
-    for(int i=0; i<10; i++){
-        printf("%d ", f[i]);
+    for(int i = 0; i<10;i++)
+    {
+        printf("%d ",num[i]);
     }
-    
     return 0;
 }
